ce3789a753c04d0968c754a10a2f9c31a69da2c7
szatyaadam <szatyaadam87@gmail.com>
2023-05-19 14:39:38 +0200

    Soldier Class and Data Request Validation

---
+++ WarGame/Models/Soldier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Soldier
+{
+
+    public Soldier(string Name, int LifePower)
+    {
+       
+        this.name = Name;
+        this.lifePower = LifePower;
+    }
+    public string name { get; set; }
+    public int lifePower { get; set; }
+}
--- WarGame/Program.cs
+++ WarGame/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace WarGame
 {
@@ -10,7 +7,33 @@ namespace WarGame
     {
         static void Main(string[] args)
         {
-            
+            Soldier Arrow = new Soldier( "Arrow", 100) ;
+            Soldier HorseRider = new Soldier("HorseRider", 150) ;
+            Soldier Swordsman  = new Soldier("Swordsman", 120) ;
+
+            Console.WriteLine("Give the numbers of the soldiers :");
+
+            #region Data request and validation
+            int Soldiers;
+            String x =Console.ReadLine();
+        
+            while (!Int32.TryParse(x, out Soldiers) ||Convert.ToInt32(x)<=1)
+            {
+               try
+                {
+                   if ( Convert.ToInt32(x) <= 1)
+                    {
+                        Console.WriteLine("The minimum numbers of the soldiers is : 2");
+                    }
+                }
+                catch
+                {
+                Console.WriteLine("Not a valid number, try again.");
+                }
+                x =Console.ReadLine();
+            }
+            #endregion
+
             Console.ReadKey();
         }
     }
