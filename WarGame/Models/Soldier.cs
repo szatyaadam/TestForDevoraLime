b0d85124c54150e4e4744b433af7c20e3eab0614
szatyaadam <szatyaadam87@gmail.com>
2023-05-20 17:04:59 +0200

    Add some decor and some repair to the lifePower method and the class of soldier

--- WarGame/Models/Soldier.cs
+++ WarGame/Models/Soldier.cs
@@ -3,12 +3,13 @@ using System.Security.Cryptography.X509Certificates;
 
 public class Soldier
 {
-    public Soldier(string Name, double LifePower)
+    public Soldier(string Name, double LifePower, int maxLifePower)
     {
         this.name = Name;
         this.lifePower = LifePower;
+        this.MaxLifePower= maxLifePower;
     }
-    
+    public int MaxLifePower { get; set; }
     public string name { get; }
     public double lifePower { get; set; }
 }
